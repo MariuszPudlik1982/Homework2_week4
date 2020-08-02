a4c3f28254a1da8964f132a49ba7e1b230ad5faa
Mariusz Pudlik <marpudlik@wp.pl>
2020-08-02 22:20:19 +0200

    Add project files.

---
+++ FisBas/FisBas.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FisBas
+{
+    public class FisBas
+    {
+       public string CheckNumber(double number)
+        {
+            string getNumber;
+            
+            if(number%3==0&&number%5==0)
+            {
+                return "FisBas";
+            }
+            if (number % 3 == 0)
+            {
+                return "Fis";
+            }
+            if (number % 5 == 0)
+            {
+                return "Bas";
+            }
+            else
+            {
+                getNumber = Convert.ToString(number);
+                return getNumber;
+            }
+            
+        }
+    }
+}
---
+++ FisBas/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FisBas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string k;
+            do
+            {              
+                double number;
+                try
+                {
+                    Console.WriteLine("Podaj liczbę rzeczywistą");
+                    number = double.Parse(Console.ReadLine());
+                    var fisBas = new FisBas();
+                    Console.WriteLine(fisBas.CheckNumber(number));
+                }
+
+                catch (FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine("Podałeś błędne dane");
+                }
+                
+                Console.WriteLine("Jeśli chcesz przerwać wciśnij k, jeśli chcesz kontynuować wciśnij dowolny znak");
+                k= Console.ReadLine();
+            } while (k!="k");
+            
+        }
+    }
+}
